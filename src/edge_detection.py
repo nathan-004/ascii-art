ee4992e5e99b209be162a6198f4ea8cff0474031
Nathan <n49874687@gmail.com>
2025-06-24 17:04:43 +0200

    feat: liste des contours avec leurs liens entre eux

--- src/edge_detection.py
+++ src/edge_detection.py
@@ -33,3 +33,41 @@ def get_edges(image_path, threshold1=100, threshold2=200, display=False):
         plt.show()
     
     return edges
+
+def get_edges_directions(image_path=None, edges=None):
+    """
+    Retourne les pixels avec des contours sous forme de direction
+
+    Parameters
+    ----------
+    image_path:str
+    edges:Numpy Array
+        Image en niveaux de gris avec 255 étant un contour
+
+    Returns
+    -------
+    liste contenant la position de chaque pixels considéré comme un contour et leurs directions
+    """
+    assert image_path is not None or edges is not None, "Aucun argument donné."
+
+    if edges is None:
+        edges = get_edges(image_path)
+    
+    def get_direction(x,y):
+        """Retourne la liste des pixels voisins qui sont des contours"""
+        neighbors = []
+        for y_modif in range(-1, 1, 1):
+            for x_modif in range(-1, 1, 1):
+                if y_modif == x_modif == 0:
+                    continue
+                if edges[y+y_modif][x+x_modif] > 0:
+                    neighbors.append((y_modif, x_modif))
+        return neighbors
+    
+    pixels = []
+    for y, line in enumerate(edges):
+        for x, color in enumerate(line):
+            if color > 0:
+                pixels.append(((x,y), get_direction(x,y)))
+    
+    return pixels
